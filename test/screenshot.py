dd792c29a3f6bd4ba3b1ac6324a7dab8a0b8b4b0
carolinekle <caroline.klewinowski@gmail.com>
2023-04-27 20:59:54 -0400

    Basic testing

--- test/screenshot.py
+++ test/screenshot.py
@@ -1,40 +1,45 @@
 import os
 import pyautogui
+from PIL import Image
+
 
 # define function to process each text file
 def process_text_file(filename):
+
     # open file 
-    os.system(f"TextEdit.txt {filename}")
+    os.system("open -a TextEdit")
     
     # wait for editor to open
     pyautogui.sleep(1)
     
     # minimize font size
     for i in range(9):
-        pyautogui.hotkey("cmd", "-")
+        pyautogui.hotkey("command", "-")
 
     # fullscreen
-    pyautogui.hotkey("ctrl", "cmd", "f")
+    pyautogui.hotkey("control", "command", "f")
     
     # zoom in
     for i in range(2):
-        pyautogui.hotkey("shift", "cmd", ".")
+        pyautogui.hotkey("shift", "command", ".")
 
     #scroll to top left
     pyautogui.moveTo(0, 0)
 
-    screenshot = pyautogui.hotkey("cmd", "5")
+    screenshot = pyautogui.screenshot()
     
     
     # save screenshot to file
     screenshot_filename = os.path.splitext(filename)[0] + ".png"
-    screenshot.save(screenshot_filename)
+    filepath = os.path.join(directory, screenshot_filename)
     
     # close text editor
-    pyautogui.hotkey("cmd", "w")
+    pyautogui.hotkey("command", "w")
 
 # iterate through all text files in directory
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".txt"):
         # process each text file
         process_text_file(filename)
+
+
